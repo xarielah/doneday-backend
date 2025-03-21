0cfdcffac3fe542200a4bbe8b275892bb7dd0c47
Ariel Aharon <xarielah@gmail.com>
2025-03-22 01:30:00 +0200

    fix static files serve

--- server.js
+++ server.js
@@ -13,20 +13,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3030;
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve('public')))
-} else {
-    const corsOptions = {
-        origin: [
-            'http://127.0.0.1:3000',
-            'http://localhost:3000',
-            'http://127.0.0.1:5173',
-            'http://localhost:5173'
-        ],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
+app.use(express.static('public'))
+
+const corsOptions = {
+    origin: [
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173'
+    ],
+    credentials: true
 }
+app.use(cors(corsOptions))
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
